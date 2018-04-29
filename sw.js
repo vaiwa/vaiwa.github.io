16d77dc4e01695f5947755b8188626439d7b60dc
Jan Návrat <jan.navrat@gmail.com>
2018-04-29 23:25:04 +0200

    last

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-934803336722dad89201.js","1087855e367081fce63eb935a48b653c"],["/commons-86f968010e8368a7da43.js","5eed8aaf9a18f3bc4c727ba7aa8e139c"],["/index.html","98535b15214b498947c49aace79be75f"],["/manifest.json","c771e75d6edcbdcba4d6794fb509a598"],["/offline-plugin-app-shell-fallback/index.html","2ea1ef6508c30fa3a43b9e9e1a7c45b5"]];
+var precacheConfig = [["/app-934803336722dad89201.js","1087855e367081fce63eb935a48b653c"],["/commons-86f968010e8368a7da43.js","5eed8aaf9a18f3bc4c727ba7aa8e139c"],["/commons-ff01c2dd6260f9faf4d8.js","c2a912ede00126484589293d88e2de64"],["/index.html","cad115c20cf7824f168b991ee55ee3e4"],["/manifest.json","c771e75d6edcbdcba4d6794fb509a598"],["/offline-plugin-app-shell-fallback/index.html","4594e2d3ab5e8764e170a36cc2501822"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
