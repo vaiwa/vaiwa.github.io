f1251b0cc5ab6603f7e5ceb5b3646c2c37844b38
Jan Návrat <jan.navrat@gmail.com>
2018-04-29 23:01:07 +0200

    last

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-934803336722dad89201.js","1087855e367081fce63eb935a48b653c"],["/commons-86f968010e8368a7da43.js","5eed8aaf9a18f3bc4c727ba7aa8e139c"],["/index.html","05d387e4386c5b1358b8f786887b5b45"],["/manifest.json","c771e75d6edcbdcba4d6794fb509a598"],["/offline-plugin-app-shell-fallback/index.html","7e1c192776a90298d4daadcf2224a309"]];
+var precacheConfig = [["/app-934803336722dad89201.js","1087855e367081fce63eb935a48b653c"],["/commons-86f968010e8368a7da43.js","5eed8aaf9a18f3bc4c727ba7aa8e139c"],["/index.html","98535b15214b498947c49aace79be75f"],["/manifest.json","c771e75d6edcbdcba4d6794fb509a598"],["/offline-plugin-app-shell-fallback/index.html","2ea1ef6508c30fa3a43b9e9e1a7c45b5"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
